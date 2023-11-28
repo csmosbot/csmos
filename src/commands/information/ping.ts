ec6989265c072683f4ea15fca2143f773222bf27
Not TCA <realtcatech@gmail.com>
2023-11-28 19:42:45 +0800

    add actual ping command

--- src/commands/information/ping.ts
+++ src/commands/information/ping.ts
@@ -1,7 +1,39 @@
 import { Command } from "@/structures/command.js";
+import { config } from "@/utils/config.js";
+import { EmbedBuilder } from "discord.js";
 
 export default new Command({
   name: "ping",
   description: "Pings the bot.",
-  run: ({ message }) => message.channel.send("Pong!"),
+  run: async ({ client, message }) => {
+    const res = await message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+          .setDescription("Pinging...")
+          .setColor(config.colors.primary),
+      ],
+    });
+
+    const ping = res.createdTimestamp - message.createdTimestamp;
+
+    res.edit({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("Pong! 🏓")
+          .setFields(
+            {
+              name: "🤖 Bot",
+              value: `${ping.toLocaleString()}ms`,
+              inline: true,
+            },
+            {
+              name: "📶 API",
+              value: `${client.ws.ping.toLocaleString()}ms`,
+              inline: true,
+            }
+          )
+          .setColor(config.colors.primary),
+      ],
+    });
+  },
 });
--- src/slashCommands/information/ping.ts
+++ src/slashCommands/information/ping.ts
@@ -1,9 +1,36 @@
 import { SlashCommand } from "@/structures/command.js";
-import { SlashCommandBuilder } from "discord.js";
+import { config } from "@/utils/config.js";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
 export default new SlashCommand({
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Pings the bot."),
-  run: ({ interaction }) => interaction.reply("Pong!"),
+  run: async ({ client, interaction }) => {
+    const res = await interaction.deferReply({
+      ephemeral: true,
+    });
+
+    const ping = res.createdTimestamp - interaction.createdTimestamp;
+
+    interaction.editReply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle("Pong! 🏓")
+          .setFields(
+            {
+              name: "🤖 Bot",
+              value: `${ping.toLocaleString()}ms`,
+              inline: true,
+            },
+            {
+              name: "📶 API",
+              value: `${client.ws.ping.toLocaleString()}ms`,
+              inline: true,
+            }
+          )
+          .setColor(config.colors.primary),
+      ],
+    });
+  },
 });
