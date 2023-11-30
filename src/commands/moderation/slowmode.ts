1de88870f499f5885098a2cab57c90804ef80f3a
Not TCA <realtcatech@gmail.com>
2023-11-30 15:50:54 +0800

    add slowmode command

---
+++ src/commands/moderation/slowmode.ts
@@ -0,0 +1,60 @@
+import { Command } from "@/structures/command.js";
+import { DangerEmbed, SuccessEmbed } from "@/utils/embed.js";
+import ms from "ms";
+
+export default new Command({
+  name: "slowmode",
+  description: "Update the slowmode for a channel.",
+  userPermissions: ["ManageChannels"],
+  run: ({ message, args }) => {
+    const length = args[0];
+    if (!length)
+      return message.channel.send({
+        embeds: [
+          new DangerEmbed().setDescription("A length must be specified."),
+        ],
+      });
+    if (ms(length) > ms("6h"))
+      return message.channel.send({
+        embeds: [
+          new DangerEmbed().setDescription(
+            "Slowmode length cannot be higher than 6 hours."
+          ),
+        ],
+      });
+
+    const channel =
+      message.mentions.channels.first() ||
+      message.guild.channels.cache.get(args[1]) ||
+      message.channel;
+    if (!channel.isTextBased() || channel.isDMBased())
+      return message.channel.send({
+        embeds: [
+          new DangerEmbed().setDescription(
+            "Channel must be a text channel in this server."
+          ),
+        ],
+      });
+
+    channel.setRateLimitPerUser(ms(length) / 1000);
+
+    if (ms(length) === 0)
+      return message.channel.send({
+        embeds: [
+          new SuccessEmbed().setDescription(
+            "The slowmode for this channel has been disabled."
+          ),
+        ],
+      });
+    else
+      return message.channel.send({
+        embeds: [
+          new SuccessEmbed().setDescription(
+            `The slowmode for this channel has been set to **${ms(ms(length), {
+              long: true,
+            })}**.`
+          ),
+        ],
+      });
+  },
+});
