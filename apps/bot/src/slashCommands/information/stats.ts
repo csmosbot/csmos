5d5312f5b8eaac60d93d931806c5b90d584bdca3
Not TCA <realtcatech@gmail.com>
2023-12-06 15:49:24 +0800

    add stats slash command

---
+++ apps/bot/src/slashCommands/information/stats.ts
@@ -0,0 +1,55 @@
+import { SlashCommand } from "@/structures/command.js";
+import { Embed } from "@/utils/embed.js";
+import { getPrefix } from "@/utils/prefix.js";
+import { SlashCommandBuilder } from "discord.js";
+
+export default new SlashCommand({
+  data: new SlashCommandBuilder()
+    .setName("stats")
+    .setDescription("View a user's statistics.")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user to view the statistics of.")
+        .setRequired(false)
+    ),
+  run: ({ client, interaction }) => {
+    const member = interaction.options.getMember("user") ?? interaction.member;
+
+    client.db.users.ensure(member.id, {
+      messages: 0,
+      characters: 0,
+    });
+
+    const data = client.db.users.get(member.id);
+    interaction.reply({
+      embeds: [
+        new Embed()
+          .setAuthor({
+            name: member.user.username,
+            iconURL: member.displayAvatarURL(),
+          })
+          .addFields(
+            {
+              name: "Leveling",
+              value: [
+                `You can also view these statistics by running \`${getPrefix(
+                  client,
+                  interaction.guild.id
+                )}rank\`.`,
+                `• **XP**: ${data.xp}`,
+                `• **Level**: ${data.level}`,
+              ].join("\n"),
+            },
+            {
+              name: "Messages",
+              value: [
+                `• **Messages**: ${data.messages}`,
+                `• **Characters**: ${data.characters}`,
+              ].join("\n"),
+            }
+          ),
+      ],
+    });
+  },
+});
