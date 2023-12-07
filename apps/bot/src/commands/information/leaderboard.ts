73f6e62089f757afb2ad007f82f85ed90989ae30
ToastedToast <50563138+ToastedDev@users.noreply.github.com>
2023-12-07 22:34:18 +0800

    feat: add leaderboard (#34)

---
+++ apps/bot/src/commands/information/leaderboard.ts
@@ -0,0 +1,117 @@
+import { Command } from "@/structures/command.js";
+import { DangerEmbed, Embed } from "@/utils/embed.js";
+import { calculateLevelXp } from "@/utils/leveling.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ComponentType,
+  type MessageReplyOptions,
+} from "discord.js";
+
+const types = {
+  xp: "XP",
+  messages: "Message",
+} as const;
+
+export default new Command({
+  name: "leaderboard",
+  description: "View the top 10 users in this server.",
+  aliases: ["lb"],
+  usage: "leaderboard <xp | messages>",
+  run: async ({ client, message, args }) => {
+    let subcommand = args[0] ?? "xp";
+    if (!["xp", "messages"].includes(subcommand))
+      return message.channel.send({
+        embeds: [
+          new DangerEmbed().setDescription(
+            "Invalid subcommand specified. Subcommand can be either `xp` or `messages`."
+          ),
+        ],
+      });
+
+    const generateEmbed = async (subcommand: string) => [
+      new Embed()
+        .setAuthor({
+          name: message.guild.name,
+          iconURL: message.guild.iconURL() ?? undefined,
+        })
+        .setTitle(`${types[subcommand as keyof typeof types]} Leaderboard`)
+        .setDescription(
+          await Promise.all(
+            client.db.users
+              .keyArray()
+              .filter((key) => key.startsWith(message.guild.id))
+              .map((key) => ({ id: key, ...client.db.users.get(key) }))
+              .sort((a, b) =>
+                subcommand === "xp"
+                  ? calculateLevelXp(a.level) +
+                    a.xp -
+                    (calculateLevelXp(b.level) + b.xp)
+                  : a.messages - b.messages
+              )
+              .map(
+                async (user, index) =>
+                  `${index}. **${
+                    (await client.users.fetch(user.id.split("-")[1])).username
+                  }** (${
+                    subcommand === "xp"
+                      ? `${user.xp} XP, level ${user.level}`
+                      : `${user.messages} messages, ${user.characters} characters`
+                  })`
+              )
+          ).then((arr) => arr.join("\n"))
+        ),
+    ];
+
+    const generateButtons = (subcommand: string) => [
+      new ActionRowBuilder<ButtonBuilder>().addComponents(
+        new ButtonBuilder()
+          .setLabel("XP")
+          .setCustomId("xp-leaderboard")
+          .setStyle(
+            subcommand === "xp" ? ButtonStyle.Primary : ButtonStyle.Secondary
+          )
+          .setDisabled(subcommand === "xp"),
+        new ButtonBuilder()
+          .setLabel("Messages")
+          .setCustomId("messages-leaderboard")
+          .setStyle(
+            subcommand === "messages"
+              ? ButtonStyle.Primary
+              : ButtonStyle.Secondary
+          )
+          .setDisabled(subcommand === "messages")
+      ),
+    ];
+
+    const options: MessageReplyOptions = {
+      embeds: await generateEmbed(subcommand),
+    };
+
+    if (!args[0]) options.components = generateButtons(subcommand);
+
+    const msg = await message.channel.send(options);
+
+    if (!args[0]) {
+      const collector = msg.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+      });
+
+      collector.on("collect", async (i) => {
+        if (
+          i.customId !== "xp-leaderboard" &&
+          i.customId !== "messages-leaderboard"
+        )
+          return;
+
+        subcommand = i.customId.split("-leaderboard")[0];
+
+        i.update({
+          embeds: await generateEmbed(subcommand),
+          components: generateButtons(subcommand),
+        });
+      });
+    }
+  },
+});
---
+++ apps/bot/src/slashCommands/information/leaderboard.ts
@@ -0,0 +1,63 @@
+import { SlashCommand } from "@/structures/command.js";
+import { Embed } from "@/utils/embed.js";
+import { SlashCommandBuilder } from "discord.js";
+
+const types = {
+  xp: "XP",
+  messages: "Message",
+} as const;
+
+export default new SlashCommand({
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("View the top 10 users in this server.")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("xp")
+        .setDescription(
+          "View the top 10 users with the most XP in this server."
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("messages")
+        .setDescription(
+          "View the top 10 users who sent the most messages in this server."
+        )
+    ),
+  run: async ({ client, interaction }) => {
+    const subcommand = interaction.options.getSubcommand();
+
+    interaction.reply({
+      embeds: [
+        new Embed()
+          .setAuthor({
+            name: interaction.guild.name,
+            iconURL: interaction.guild.iconURL() ?? undefined,
+          })
+          .setTitle(`${types[subcommand as keyof typeof types]} Leaderboard`)
+          .setDescription(
+            await Promise.all(
+              client.db.users
+                .keyArray()
+                .filter((key) => key.startsWith(interaction.guild.id))
+                .map((key) => ({ id: key, ...client.db.users.get(key) }))
+                .sort((a, b) =>
+                  subcommand === "xp" ? a.xp - b.xp : a.messages - b.messages
+                )
+                .map(
+                  async (user, index) =>
+                    `${index}. **${
+                      (await client.users.fetch(user.id.split("-")[1])).username
+                    }** (${
+                      subcommand === "xp"
+                        ? `${user.xp} XP, level ${user.level}`
+                        : `${user.messages} messages, ${user.characters} characters`
+                    })`
+                )
+            ).then((arr) => arr.join("\n"))
+          ),
+      ],
+    });
+  },
+});
