d5deb197b40d7d7e47137a40b31c4fb4b8225291
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-09 14:47:40 +0800

    seoify

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -7,6 +7,52 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "csmos",
+      description:
+        "Everything you need to build the Discord server of your dreams, all in one bot. No need to add any more bots.",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            href: "/apple-icon-180x180.png",
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            href: "/android-icon-192x192.png",
+            rel: "icon",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            href: "/favicon-32x32.png",
+            rel: "icon",
+            sizes: "32x32",
+            type: "image/png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            href: "/favicon-16x16.png",
+            rel: "icon",
+            sizes: "16x16",
+            type: "image/png",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            name: "theme-color",
+            content: "#F600E0",
+          },
+        },
+      ],
       logo: {
         src: "./src/assets/csmos.webp",
         replacesTitle: true,
