b7e4f2717b2c173b641da228c553f8dc58d02003
Not TCA <50563138+ToastedDev@users.noreply.github.com>
2023-12-09 11:04:33 +0800

    convert docs to astro

---
+++ apps/web/tailwind.config.mjs
@@ -0,0 +1,22 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+import colors from "tailwindcss/colors";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
+        accent: colors.fuchsia,
+        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+        gray: colors.zinc,
+      },
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [starlightPlugin()],
+};
--- packages/eslint-config/nextjs.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ["./index.js", "plugin:@next/next/recommended"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-};
