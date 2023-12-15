6c4aa3b75e7d46a06ddc7d5954c40db4ba707302
ToastedToast <50563138+ToastedDev@users.noreply.github.com>
2023-12-15 13:59:13 +0800

    feat(join-msg): add support server (#88)
    closes #87.

--- apps/bot/src/events/guildCreate.ts
+++ apps/bot/src/events/guildCreate.ts
@@ -2,7 +2,12 @@ import { Event } from "@/structures/event";
 import { config } from "@/utils/config";
 import { Embed } from "@/utils/embed";
 import { createGuild } from "@csmos/db";
-import { ChannelType } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChannelType,
+} from "discord.js";
 
 export default new Event({
   name: "guildCreate",
@@ -10,20 +15,31 @@ export default new Event({
     await createGuild(guild.id);
 
     function sendMessage(id: string) {
-      const embed = new Embed()
-        .setAuthor({
-          name: client.user.username,
-          iconURL: client.user.displayAvatarURL(),
-        })
-        .setTitle("Hello there! 👋")
-        .setDescription(
-          `Thanks for inviting me to your server! I hope I can help you turn this into the Discord server of your dreams!\n\nBy default, my prefix is \`${config.prefix}\`. To get started, type \`${config.prefix}help\` to view all my commands.`
-        );
-
       const channel = guild.channels.cache.get(id);
       if (!channel || !channel.isTextBased()) return;
 
-      return channel.send({ embeds: [embed] });
+      return channel.send({
+        embeds: [
+          new Embed()
+            .setAuthor({
+              name: client.user.username,
+              iconURL: client.user.displayAvatarURL(),
+            })
+            .setTitle("Hello there! 👋")
+            .setDescription(
+              `Thanks for inviting me to your server! I hope I can help you turn this into the Discord server of your dreams!\n\nBy default, my prefix is \`${config.prefix}\`. To get started, type \`${config.prefix}help\` to view all my commands.`
+            ),
+        ],
+        components: [
+          new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+              .setLabel("Join the support server")
+              .setEmoji("<:discord:1043438510616608779>")
+              .setURL("https://discord.com/invite/q7WNcPakYh")
+              .setStyle(ButtonStyle.Link)
+          ),
+        ],
+      });
     }
 
     if (guild.publicUpdatesChannelId) sendMessage(guild.publicUpdatesChannelId);
