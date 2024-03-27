cf75ea41716cb3d6020c84ce9d2fdbbb0e48b9f7
ToastedToast <hey@toasted.dev>
2024-03-27 12:24:12 +0800

    update landing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-primary text-4xl font-bold">hello world</h1>;
+  return (
+    <div className="container py-6">
+      <h1>hello world</h1>
+    </div>
+  );
 }
