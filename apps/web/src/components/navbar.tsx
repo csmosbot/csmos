5bb2d48bbdeddf3f53d65288aa40c5fd19650802
ToastedToast <hey@toasted.dev>
2024-03-27 11:54:41 +0800

    move improvements

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -7,14 +7,14 @@ import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { buttonVariants } from "./ui/button";
-import { Github, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 
 export function Navbar() {
   const pathname = usePathname();
 
   return (
     <header className="sticky top-0 z-50 bg-background/90 backdrop-blur-sm container border-b py-2 flex items-center justify-between">
-      <nav className="flex items-center gap-3">
+      <nav className="flex items-center gap-4">
         <Link
           href="/"
           className="flex items-center gap-2 hover:opacity-80 transition-all"
@@ -26,7 +26,7 @@ export function Navbar() {
             key={index}
             href={item.href}
             className={cn(
-              "text-[15px] hover:underline transition-colors hover:text-foreground/80",
+              "text-[14.5px] hover:underline transition-colors hover:text-foreground/80",
               pathname.startsWith(item.href)
                 ? "font-medium"
                 : "text-foreground/60"
