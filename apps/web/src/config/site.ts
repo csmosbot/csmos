cea2863d85b37c55bc3103e35c924297fed3befb
ToastedToast <hey@toasted.dev>
2024-03-27 11:53:26 +0800

    improve header

--- apps/web/src/config/site.ts
+++ apps/web/src/config/site.ts
@@ -1,10 +1,18 @@
 import type { ItemWithHref } from "@/lib/types/item";
 
 interface SiteConfig {
+  links: {
+    invite: string;
+    github: string;
+  };
   navbarItems: ItemWithHref[];
 }
 
 export const siteConfig: SiteConfig = {
+  links: {
+    invite: "/invite",
+    github: "https://github.com/csmosbot/csmos",
+  },
   navbarItems: [
     {
       title: "Docs",
