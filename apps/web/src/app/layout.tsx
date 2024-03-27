aac2f4167d5c054362dba387ce90d5377f6979ac
ToastedToast <hey@toasted.dev>
2024-03-27 15:02:07 +0800

    use geist

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { ThemeProvider } from "@/components/themes";
 import { Navbar } from "@/components/navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
