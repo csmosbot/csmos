63027a174f9367bb12498ca86328bcacef3c30c2
ToastedToast <hey@toasted.dev>
2024-03-30 10:36:32 +0800

    fix(bot): prepare for production (#276)

---
+++ apps/bot/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  format: ["cjs"],
+  entry: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.ts*",
+    "src/**/*.tsx",
+  ],
+  clean: true,
+  legacyOutput: true,
+  outDir: "dist",
+  skipNodeModulesBundle: true,
+  tsconfig: "tsconfig.json",
+  noExternal: ["@csmos/db"],
+});
